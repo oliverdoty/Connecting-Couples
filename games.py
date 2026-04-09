544e24d0c3e44833763cb315783e1b40255c8215
oliverdoty <oliverdoty2@gmail.com>
2026-04-09 01:00:46 +0000

    Wed, Apr 8, 2026, 9:00 PM -04:00

--- games.py
+++ games.py
@@ -4,24 +4,24 @@ def twentyquestions():
 def custom_wordle():
     secret = list(input('What is the key word? ').lower()) # all lowercase for consistency
     while True:
-        if secret.isalpha() == False: # no length restricitons on secret word
-            secret = input('Please enter a key word consisting of only characters in the alphabet: ').lower()
+        if ''.join(secret).isalpha() == False: # no length restricitons on secret word
+            secret = list(input('Please enter a key word consisting of only characters in the alphabet: ').lower())
         else:
             break
     length = len(secret)
     for i in range(6):
         guess = list(input('What is your guess? ').lower())
         while True:
-            if guess != length or guess.isalpha() == False:
-                guess = input(f'Input a guess with {length} letters: ')
+            if len(guess) != length or ''.join(guess).isalpha() == False:
+                guess = list(input(f'Input a guess with {length} letters: '))
             else:
                 break
-        for j in range(secret):
-            for k in range(guess):
+        for j in range(len(secret)):
+            for k in range(len(guess)):
                 if guess[k] == secret[j]:
                     guess[k] = '\033[33m' + guess[k] + '\033[0m' # makes yellow
                     if j == k:
-                        guess[k] = '\033[32m', guess[k], + '\033[0m' # makes green
+                        guess[k] = '\033[32m' + guess[k] + '\033[0m' # makes green
         print(''.join(guess))
 
 def tanks():
