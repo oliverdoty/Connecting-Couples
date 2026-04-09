2779e4f564c9e2c3662b08122890f5b712270458
oliverdoty <oliverdoty2@gmail.com>
2026-04-09 00:50:30 +0000

    Wed, Apr 8, 2026, 8:50 PM -04:00

--- games.py
+++ games.py
@@ -2,23 +2,26 @@ def twentyquestions():
     return
 
 def custom_wordle():
-    secret = input('What is the key word? ')
-    digits = len(secret)
+    secret = list(input('What is the key word? ').lower()) # all lowercase for consistency
+    while True:
+        if secret.isalpha() == False: # no length restricitons on secret word
+            secret = input('Please enter a key word consisting of only characters in the alphabet: ').lower()
+        else:
+            break
+    length = len(secret)
     for i in range(6):
-        guess = input('What is your guess? ')
+        guess = list(input('What is your guess? ').lower())
         while True:
-            if guess != digits:
-                guess = input(f'Input a guess with {digits}: ')
-            elif guess.alpha() == False:
-                guess = input('You guess must be all letters: ')
+            if guess != length or guess.isalpha() == False:
+                guess = input(f'Input a guess with {length} letters: ')
             else:
                 break
-        for j in range(list(guess)):
-            for k in range(list(secret)):
-                if list(guess)[j] == list(secret)[k]:
-                    #yellow
+        for j in range(guess):
+            for k in range(secret):
+                if guess[j] == secret[k]:
+                    guess[j] = k
                     if j == k:
-                        pass#green
+                        pass #green
 
                         # ....
     return
