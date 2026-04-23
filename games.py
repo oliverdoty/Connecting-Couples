d04f4cb6ec20f123cf796c4634816332ad4984b2
oliverdoty <oliverdoty2@gmail.com>
2026-04-23 02:42:51 +0000

    Wed, Apr 22, 2026, 10:42 PM -04:00

--- games.py
+++ games.py
@@ -63,26 +63,26 @@ def custom_wordle_server():
             conn2client, addr = s.accept()
             print('Connected by', addr)
 
-        with conn2client:
-            for i in range(6):
-                guess = s.recv(1024)
-                if guess == '':
-                    break
-                green_letters = 0
-                for j in range(len(secret)):
-                    for k in range(len(guess)):
-                        if guess[k] == secret[j]:
-                            if '033' not in guess[k]:
-                                guess[k] = '\033[33m' + guess[k] + '\033[0m' # makes yellow
-                            if j == k:
-                                guess[k] = '\033[32m' + guess[k][5] + '\033[0m' # makes green
-                                green_letters += 1
-                accuracy = ''.join(guess)
-                if green_letters == len(secret):
-                    s.sendall(0)
-                    break
-                else:
-                    s.sendall(accuracy)
+            with conn2client:
+                for i in range(6):
+                    guess = s.recv(1024)
+                    if guess == '':
+                        break
+                    green_letters = 0
+                    for j in range(len(secret)):
+                        for k in range(len(guess)):
+                            if guess[k] == secret[j]:
+                                if '033' not in guess[k]:
+                                    guess[k] = '\033[33m' + guess[k] + '\033[0m' # makes yellow
+                                if j == k:
+                                    guess[k] = '\033[32m' + guess[k][5] + '\033[0m' # makes green
+                                    green_letters += 1
+                    accuracy = ''.join(guess)
+                    if green_letters == len(secret):
+                        s.sendall(0)
+                        break
+                    else:
+                        s.sendall(accuracy)
 
 
     return
