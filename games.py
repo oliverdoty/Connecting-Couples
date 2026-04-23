8d5f21362ff9fc533fd17f96e10fdbbc767e7081
oliverdoty <oliverdoty2@gmail.com>
2026-04-23 02:34:58 +0000

    Wed, Apr 22, 2026, 10:34 PM -04:00

--- games.py
+++ games.py
@@ -64,7 +64,19 @@ def custom_wordle_server():
         print('Connected by', addr)
 
         with conn2client:
-
+            green_letters = 0
+            for j in range(len(secret)):
+                for k in range(len(guess)):
+                    if guess[k] == secret[j]:
+                        if '033' not in guess[k]:
+                            guess[k] = '\033[33m' + guess[k] + '\033[0m' # makes yellow
+                        if j == k:
+                            guess[k] = '\033[32m' + guess[k][5] + '\033[0m' # makes green
+                            green_letters += 1
+            print(''.join(guess))
+            if green_letters == len(secret):
+                return True
+        return False
 
 
     return
@@ -108,19 +120,7 @@ def custom_wordle_client_d():
                     guess = list(input(f'Input a guess with {length} letters: '))
                 else:
                     break
-            green_letters = 0
-            for j in range(len(secret)):
-                for k in range(len(guess)):
-                    if guess[k] == secret[j]:
-                        if '033' not in guess[k]:
-                            guess[k] = '\033[33m' + guess[k] + '\033[0m' # makes yellow
-                        if j == k:
-                            guess[k] = '\033[32m' + guess[k][5] + '\033[0m' # makes green
-                            green_letters += 1
-            print(''.join(guess))
-            if green_letters == len(secret):
-                return True
-        return False
+            
 
     return
 
